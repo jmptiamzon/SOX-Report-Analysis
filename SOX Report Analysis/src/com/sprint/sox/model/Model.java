596b207d0d3b13ad4f18f342628a507e53ad2669
Pogi <jmptiamzon@gmail.com>
2021-04-24 15:00:53 +0800

    Removed cred

--- SOX Report Analysis/src/com/sprint/sox/model/Model.java
+++ SOX Report Analysis/src/com/sprint/sox/model/Model.java
@@ -28,7 +28,7 @@ public class Model {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:@POC0365.corp.sprint.com:1521/OIMP101", "apps_query", "apps_query");
+					DriverManager.getConnection("", "", "");
 			
 			PreparedStatement statement = conn.prepareStatement("SELECT * FROM (SELECT DISTINCT A.TRANSACTION_NUMBER, A.TRANSACTION_TYPE, A.SKUID, A.WAREHOUSE, A.LOCATION, A.STATUS, A.ERROR_CODE, B.ERROR_CODE||'-'||B.ERROR_MESSAGE COMMENTS, ROW_NUMBER() OVER(PARTITION BY A.TRANSACTION_NUMBER, A.TRANSACTION_TYPE, A.SKUID, A.WAREHOUSE, A.LOCATION ORDER BY A.LAST_UPDATE_DATE desc)row1 FROM  APPS.SPRN_IM_WM_TRX_IN_INT A, SPRN.SPRN_COMMON_ERROR_TXN B WHERE A.RECORD_ID = B.RECORD_ID (+) AND A.TRANSACTION_NUMBER IN (" + parameters + ")) WHERE ROW1 = 1");
 			
@@ -71,7 +71,7 @@ public class Model {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:@POC0365.corp.sprint.com:1521/OIMP101", "apps_query", "apps_query");
+					DriverManager.getConnection("", "", "");
 			
 			PreparedStatement statement = conn.prepareStatement("SELECT * FROM (SELECT DISTINCT A.UPS_REF_NO, A.ORIG_ORD_NUMBER, A.ITEM_ID, A.STATUS, A.ERROR_CODE, A.LAST_UPDATE_DATE, ROW_NUMBER() OVER(PARTITION BY A.UPS_REF_NO, A.ORIG_ORD_NUMBER, A.ITEM_ID ORDER BY A.LAST_UPDATE_DATE desc)row1 FROM SPRN.SPRN_RMA_BATCH_IN_INT A WHERE 1=1 AND A.UPS_REF_NO IN(" + parameters + ")) WHERE ROW1 = 1");
 			
@@ -112,7 +112,7 @@ public class Model {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:@POC0365.corp.sprint.com:1521/OIMP101", "apps_query", "apps_query");
+					DriverManager.getConnection("", "", "");
 			
 			PreparedStatement statement = conn.prepareStatement("SELECT * FROM (SELECT DISTINCT a.TRANSACTION_NUMBER, a.skuid, a.reference1, a.STATUS, a.error_code, b.error_code||'-'|| b.error_message \"COMMENTS\", a.last_update_date, ROW_NUMBER() OVER(PARTITION BY A.transaction_number, a.skuid, a.reference1 ORDER BY A.LAST_UPDATE_DATE desc)row1 FROM apps.sprn_rcv_trans_in_int a, SPRN.SPRN_COMMON_ERROR_TXN b WHERE a.record_id = b.record_id(+) and a.TRANSACTION_NUMBER IN(" + parameters + ")) WHERE ROW1 = 1");
 			
@@ -154,7 +154,7 @@ public class Model {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:@POC0365.corp.sprint.com:1521/OIMP101", "apps_query", "apps_query");
+					DriverManager.getConnection("", "", "");
 			
 			PreparedStatement statement = conn.prepareStatement("SELECT * FROM (SELECT DISTINCT A.TRANSACTION_NUMBER, A.SKU_ID, A.WAREHOUSE, A.LOCATION, A.STATUS, B.ERROR_CODE||'-'||B.ERROR_MESSAGE, a.last_update_date, ROW_NUMBER() OVER(PARTITION BY A.TRANSACTION_NUMBER, A.SKU_ID, A.WAREHOUSE, A.LOCATION ORDER BY A.LAST_UPDATE_DATE desc)row1 FROM APPS.SPRN_WM_TRX_RESKU_TXN_IN_INT A, SPRN.SPRN_COMMON_ERROR_TXN B  WHERE A.RECORD_ID = B.RECORD_ID (+) AND A.TRANSACTION_NUMBER IN(" + parameters + ")) WHERE ROW1 = 1");
 			
@@ -193,7 +193,7 @@ public class Model {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection conn = 
-					DriverManager.getConnection("jdbc:oracle:thin:@POC0365.corp.sprint.com:1521/OIMP101", "apps_query", "apps_query");
+					DriverManager.getConnection("", "", "");
 			
 			PreparedStatement statement = conn.prepareStatement("SELECT * FROM (SELECT DISTINCT A.PICKTICKET_CONTROL_NUMBER, A.BATCH_ID, A.STATUS, A.ERROR_CODE, B.ERROR_CODE || '-' || B.ERROR_MESSAGE \"COMMENTS\" , A.LAST_UPDATE_DATE, ROW_NUMBER() OVER(PARTITION BY A.PICKTICKET_CONTROL_NUMBER, A.BATCH_ID ORDER BY A.LAST_UPDATE_DATE desc)row1 FROM APPS.SPRN_OM_SHIP_HDR_IN_INT A, SPRN.SPRN_COMMON_ERROR_TXN B WHERE A.RECORD_ID = B.RECORD_ID (+) AND A.PICKTICKET_CONTROL_NUMBER IN(" + parameters + ")) WHERE ROW1 = 1");
 			
